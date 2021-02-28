f87b37fcebf1af9c3991e7a5c30ded5fe5183935
odhier <odhier1@gmail.com>
2021-03-01 03:55:07 +0700

    import modal

--- resources/views/livewire/partials/modals/admin/import-nilai.blade.php
+++ resources/views/livewire/partials/modals/admin/import-nilai.blade.php
@@ -37,15 +37,16 @@
             </div>
         </div>
     </div>
-    {{$isUploading}}
-    <div id="loadingStatement" wire:loading>
-        <div wire:loading class="fixed top-0 left-0 right-0 bottom-0 w-full h-screen z-50 overflow-hidden bg-gray-700 opacity-75 flex flex-col items-center justify-center">
+
+    <div wire:loading>
+    <div id="loadingStatement">
+        <div class="fixed top-0 left-0 right-0 bottom-0 w-full h-screen z-50 overflow-hidden bg-gray-700 opacity-75 flex flex-col items-center justify-center">
             <div class="loader ease-linear rounded-full border-4 border-t-4 border-gray-200 h-12 w-12 mb-4"></div>
             <h2 class="text-center text-white text-xl font-semibold">Uploading File...</h2>
             <p class="w-1/3 text-center text-white">This may take a few seconds, please don't close this page.</p>
         </div>
-
     </div>
+</div>
 </div>
 
 
